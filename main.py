0e3ee59234543107010ce3e463e07b35a8d5189c
Anastasiia Nikulova <anastasianikulova4@gmail.com>
2026-05-31 13:02:00 +0300

    Дополнительные правки в main.py

--- main.py
+++ main.py
@@ -16,3 +16,26 @@ def test_access_uppercase():
     assert get_category("ПАРОЛЬ ПОТЕРЯН") == "Запросы на предоставление доступа"
 
 
+def test_incident_word_error():
+    assert get_category("Ошибка при входе") == "Сообщения о внештатных ситуациях"
+def test_incident_word_work():
+    assert get_category("Система не работает") == "Сообщения о внештатных ситуациях"
+def test_incident_word_starts():
+    assert get_category("Программа не запускается") == "Сообщения о внештатных ситуациях"
+def test_incident_word_open():
+    assert get_category("Тз не открывается") == "Сообщения о внештатных ситуациях"
+def test_incident_word_cant():
+    assert get_category("Я не могу войти") == "Сообщения о внештатных ситуациях"
+def test_incident_word_unavailable():
+    assert get_category("Сайт был недоступен вчера") == "Сообщения о внештатных ситуациях"
+def test_incident_word_freezes():
+    assert get_category("Прога зависает постоянно") == "Сообщения о внештатных ситуациях"
+def test_incident_uppercase():
+    assert get_category("СИСТЕМА НЕ РАБОТАЕТ") == "Сообщения о внештатных ситуациях"
+
+
+
+
+
+
+
